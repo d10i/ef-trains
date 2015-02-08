3571c55f930ff4b5cd2d608b1202fada484dbcd7
Dario Simonetti <dario.simonetti@ovoenergy.com>
2015-02-08 13:09:29 +0000

    Changed config for AWS

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp -i ~/.ssh/eftrains.pem configuration.yml ec2-user@ec2-54-77-127-14.eu-west-1.compute.amazonaws.com:/etc/ef-trains/configuration.yml
+scp -i ~/.ssh/eftrains.pem configuration-aws.yml ec2-user@ec2-54-77-127-14.eu-west-1.compute.amazonaws.com:/etc/ef-trains/configuration.yml
 scp -i ~/.ssh/eftrains.pem target/ef-trains-1.0-SNAPSHOT.jar  ec2-user@ec2-54-77-127-14.eu-west-1.compute.amazonaws.com:/etc/ef-trains/ef-trains-1.0-SNAPSHOT.jar
