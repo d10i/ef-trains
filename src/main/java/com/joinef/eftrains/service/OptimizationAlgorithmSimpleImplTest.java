701f346887743da22fafc4de65ac54156a150e2a
Dario Simonetti <dario.simonetti@ovoenergy.com>
2015-02-08 11:55:13 +0000

    Changed int to String in test

--- src/main/java/com/joinef/eftrains/service/OptimizationAlgorithmSimpleImplTest.java
+++ src/main/java/com/joinef/eftrains/service/OptimizationAlgorithmSimpleImplTest.java
@@ -1,5 +1,6 @@
 package com.joinef.eftrains.service;
 
+import com.joinef.eftrains.dao.StationDao;
 import com.joinef.eftrains.entity.Journey;
 import junit.framework.Assert;
 import org.joda.time.DateTime;
@@ -26,6 +27,9 @@ public class OptimizationAlgorithmSimpleImplTest {
     @Mock
     private JourneyService journeyService;
 
+    @Mock
+    private StationDao stationDao;
+
     private OptimizationAlgorithmSimpleImpl optimizationAlgorithm;
 
     @Before
@@ -43,46 +47,46 @@ public class OptimizationAlgorithmSimpleImplTest {
     @Test
     public void testPerformOptimization() throws Exception {
 
-        when(journeyService.countStations()).thenReturn(4);
+        when(stationDao.count()).thenReturn(4);
 
-        when(journeyService.findFrom(eq(0), any(DateTime.class))).thenAnswer(new Answer<HashMap<Integer, Journey>>() {
+        when(journeyService.findFrom(eq("0"), any(DateTime.class))).thenAnswer(new Answer<HashMap<Integer, Journey>>() {
             @Override
             public HashMap<Integer, Journey> answer(InvocationOnMock invocationOnMock) throws Throwable {
                 HashMap<Integer, Journey> journeys = new HashMap<Integer, Journey>();
-                journeys.put(1, new Journey(1, 0, 1, null, null));
-                journeys.put(2, new Journey(7, 0, 2, null, null));
+                journeys.put(1, new Journey.Builder().departureStation("0").arrivalStation("1").price(1).build());
+                journeys.put(2, new Journey.Builder().departureStation("0").arrivalStation("2").price(7.0f).build());
                 return journeys;
             }
         });
 
-        when(journeyService.findFrom(eq(1), any(DateTime.class))).thenAnswer(new Answer<HashMap<Integer, Journey>>() {
+        when(journeyService.findFrom(eq("1"), any(DateTime.class))).thenAnswer(new Answer<HashMap<Integer, Journey>>() {
             @Override
             public HashMap<Integer, Journey> answer(InvocationOnMock invocationOnMock) throws Throwable {
                 HashMap<Integer, Journey> journeys = new HashMap<Integer, Journey>();
-                journeys.put(0, new Journey(1, 1, 0, null, null));
-                journeys.put(2, new Journey(1, 1, 2, null, null));
-                journeys.put(3, new Journey(7, 1, 3, null, null));
+                journeys.put(0, new Journey.Builder().departureStation("1").arrivalStation("0").price(1.0f).build());
+                journeys.put(2, new Journey.Builder().departureStation("1").arrivalStation("2").price(1.0f).build());
+                journeys.put(3, new Journey.Builder().departureStation("1").arrivalStation("3").price(7.0f).build());
                 return journeys;
             }
         });
 
-        when(journeyService.findFrom(eq(2), any(DateTime.class))).thenAnswer(new Answer<HashMap<Integer, Journey>>() {
+        when(journeyService.findFrom("2", any(DateTime.class))).thenAnswer(new Answer<HashMap<Integer, Journey>>() {
             @Override
             public HashMap<Integer, Journey> answer(InvocationOnMock invocationOnMock) throws Throwable {
                 HashMap<Integer, Journey> journeys = new HashMap<Integer, Journey>();
-                journeys.put(0, new Journey(7, 2, 0, null, null));
-                journeys.put(3, new Journey(1, 2, 3, null, null));
-                journeys.put(1, new Journey(1, 2, 1, null, null));
+                journeys.put(0, new Journey.Builder().departureStation("2").arrivalStation("0").price(7.0f).build());
+                journeys.put(3, new Journey.Builder().departureStation("2").arrivalStation("3").price(1.0f).build());
+                journeys.put(1, new Journey.Builder().departureStation("2").arrivalStation("1").price(1.0f).build());
                 return journeys;
             }
         });
 
-        when(journeyService.findFrom(eq(3), any(DateTime.class))).thenAnswer(new Answer<HashMap<Integer, Journey>>() {
+        when(journeyService.findFrom("3", any(DateTime.class))).thenAnswer(new Answer<HashMap<Integer, Journey>>() {
             @Override
             public HashMap<Integer, Journey> answer(InvocationOnMock invocationOnMock) throws Throwable {
                 HashMap<Integer, Journey> journeys = new HashMap<Integer, Journey>();
-                journeys.put(1, new Journey(7, 3, 1, null, null));
-                journeys.put(2, new Journey(1, 3, 2, null, null));
+                journeys.put(1, new Journey.Builder().departureStation("3").arrivalStation("1").price(7.0f).build());
+                journeys.put(2, new Journey.Builder().departureStation("3").arrivalStation("2").price(1.0f).build());
                 return journeys;
             }
         });
@@ -114,19 +118,22 @@ public class OptimizationAlgorithmSimpleImplTest {
         //when(journeyDao.countStations()).thenReturn(testCount);
         //journeyService = new JourneyServiceImpl(false);
 
-        when(journeyService.findFrom(anyInt(), any(DateTime.class))).thenAnswer(new Answer<List<Journey>>() {
+        when(journeyService.findFrom(anyString(), any(DateTime.class))).thenAnswer(new Answer<List<Journey>>() {
             @Override
             public List<Journey> answer(InvocationOnMock invocationOnMock) throws Throwable {
                 int i = (Integer) invocationOnMock.getArguments()[0];
 
-                List<Journey> journeys = new ArrayList<Journey>();
-
-                if (i + 1 <= testCount - 1) journeys.add(new Journey(7.0f, i, i + 1, null, null));
-                if (i + 2 <= testCount - 1) journeys.add(new Journey(6.0f, i, i + 2, null, null));
-                if (i + 3 <= testCount - 1) journeys.add(new Journey(5.0f, i, i + 3, null, null));
-                if (i + 4 <= testCount - 1) journeys.add(new Journey(8.0f, i, i + 4, null, null));
-                if (i + 5 <= testCount - 1) journeys.add(new Journey(10.0f, i, i + 5, null, null));
-
+                List<Journey> journeys = new ArrayList<>();
+                if (i + 1 <= testCount - 1)
+                    journeys.add(new Journey.Builder().departureStation(String.valueOf(i)).arrivalStation(String.valueOf(i + 1)).price(7.0f).build());
+                if (i + 2 <= testCount - 1)
+                    journeys.add(new Journey.Builder().departureStation(String.valueOf(i)).arrivalStation(String.valueOf(i + 2)).price(6.0f).build());
+                if (i + 3 <= testCount - 1)
+                    journeys.add(new Journey.Builder().departureStation(String.valueOf(i)).arrivalStation(String.valueOf(i + 3)).price(5.0f).build());
+                if (i + 4 <= testCount - 1)
+                    journeys.add(new Journey.Builder().departureStation(String.valueOf(i)).arrivalStation(String.valueOf(i + 4)).price(8.0f).build());
+                if (i + 5 <= testCount - 1)
+                    journeys.add(new Journey.Builder().departureStation(String.valueOf(i)).arrivalStation(String.valueOf(i + 5)).price(10.0f).build());
 
                 return journeys;
             }
@@ -145,7 +152,7 @@ public class OptimizationAlgorithmSimpleImplTest {
             }
         }*/
 
-        when(journeyService.countStations()).thenReturn(testCount);
+        when(stationDao.count()).thenReturn(testCount);
 
         List<List<Journey>> journeys =  optimizationAlgorithm.performOptimization(0, testCount - 1);
         List<Journey> route = journeys.get(0);
